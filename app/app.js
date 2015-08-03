5a260c30b6fe91b9961a48f8d9c0da78b613957f
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-03 12:04:21 -0700

    Draft flash-card like interface with 4 options

---
+++ app/app.js
@@ -0,0 +1,10 @@
+var marcoApp = angular.module('marcoApp',[]);
+
+marcoApp.controller('QuestionCtrl', ['$scope', function($scope){
+  $scope.question = 'placeholder for picture of Marco';
+  $scope.option1 = 'Marco';
+  $scope.option2 = 'Bob';
+  $scope.option3 = 'John';
+  $scope.option4 = 'Tom';
+
+}]);
