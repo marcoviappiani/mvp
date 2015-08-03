02b1c6bc19ad3a31bcc09e65966fecae4e1f733b
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-03 12:12:04 -0700

    Can detect if answer is correct

--- app/app.js
+++ app/app.js
@@ -2,13 +2,16 @@ var marcoApp = angular.module('marcoApp',[]);
 
 marcoApp.controller('QuestionCtrl', ['$scope', function($scope){
   $scope.question = 'placeholder for picture of Marco';
+  
   $scope.option1 = 'Marco';
   $scope.option2 = 'Bob';
   $scope.option3 = 'John';
   $scope.option4 = 'Tom';
-
+  
+  $scope.solution = 'Marco';
 
   $scope.choose = function(selection) {
     $scope.selection = selection;
+    $scope.isCorrect = $scope.selection === $scope.solution;
   }
 }]);
