6d968ae2e0b9e867d9e65ba7369dfabb16c268af
Marco Viappiani <marco.viappiani@gmail.com>
2015-08-03 13:00:26 -0700

    Refactored to create QuestionGenerator Factory service

--- app/app.js
+++ app/app.js
@@ -1,17 +1,44 @@
 var marcoApp = angular.module('marcoApp',[]);
 
-marcoApp.controller('QuestionCtrl', ['$scope', function($scope){
-  $scope.question = 'placeholder for picture of Marco';
-  
-  $scope.option1 = 'Marco';
-  $scope.option2 = 'Bob';
-  $scope.option3 = 'John';
-  $scope.option4 = 'Tom';
-  
-  $scope.solution = 'Marco';
+marcoApp.controller('QuestionCtrl', ['$scope', 'QuestionGenerator', function($scope, QuestionGenerator){
+
+  $scope.question = QuestionGenerator.generateQuestion(); 
+  // console.log($scope.question);
 
   $scope.choose = function(selection) {
     $scope.selection = selection;
-    $scope.isCorrect = $scope.selection === $scope.solution;
+    $scope.isCorrect = $scope.selection === $scope.question.solution;
   }
+
+  // $scope.question = 'placeholder for picture of Marco';  
+  // $scope.option1 = 'Marco';
+  // $scope.option2 = 'Bob';
+  // $scope.option3 = 'John';
+  // $scope.option4 = 'Tom';
+  // $scope.solution = 'Marco';
+
 }]);
+
+
+marcoApp.factory('QuestionGenerator',function(){
+  var optionNum = 4;
+
+  var generateQuestion = function() {
+    var question = {};
+    question.options = ['Marco', 'Bob', 'John', 'Tom'];
+    question.image = 'placeholder for picture of Marco';
+    question.solution = 'Marco';
+
+    // question.choose = function(selection) {
+    //   return question.solution === selection;
+    // };
+
+    return question;
+  };
+
+  return {  
+    generateQuestion: generateQuestion
+  };
+
+});
+
